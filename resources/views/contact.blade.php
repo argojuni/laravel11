99371c37bdffdf45d93ddd0e54eb736d5d270c1f
argojuni <juniargoprasaja05@gmail.com>
2024-11-09 16:20:01 +0700

    Blade Template Engine

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,16 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman About</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman Contact</title>
 </head>
 <body>
     <a href="/">Home</a>
     <a href="/blog">Blog</a>
     <a href="/about">About</a>
     <a href="/contact">Contact</a>
-    <h1>Halaman About</h1>
-    <h3>Owner : {{ $nama }}</h3>
-    <img src="img/dprdlogo.png" alt="DPRD KAB. BLITAR" width="300">
+
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            <a href="#">Instagram</a>
+        </li>
+        <li>
+            <a href="#">X</a>
+        </li>
+        <li>
+            <a href="#">Youtube</a>
+        </li>
+    </ul>
 </body>
 </html>
